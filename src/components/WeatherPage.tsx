f6cde9a442d80b11fe4d8f2641469c26c255530f
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-10-14 14:00:13 +0200

    added details

--- src/components/WeatherPage.tsx
+++ src/components/WeatherPage.tsx
@@ -3,28 +3,31 @@ import FetchingFunction, { Weather } from "../lib/FetchingFunction"
 
 export default function WeatherPage(){
 	const [weatherData, setWeatherData] = useState<Weather>();
-    const [searchText, setSearchText] = useState("")
+    const [searchText, setSearchText] = useState("Berlin")
     const [checkSubmit, setSubmit] = useState(false)
 
-	const query = "Berlin";
+	// const query = "Berlin";
 	const language = "de";
     
 	useEffect(() => {
-		const fetchWeather  = async () => setWeatherData(await FetchingFunction(query, language));
+		const fetchWeather  = async () => setWeatherData(await FetchingFunction(searchText, language));
 		fetchWeather();
-	}, [query, ]);
+	}, [checkSubmit]);
 
-	const check = weatherData?.main.temp
+	const temperature = weatherData?.main.temp
+    const description = weatherData?.weather[0].description
+    const windSpeed = weatherData?.wind.speed
     return(
         <div>
         <div>
             <input onChange={(event)=>{
                 setSearchText(event.target.value)}} value={searchText}
             type="text" aria-label="Eingabefeld für Stadt" placeholder="Stadt" />
-            <button>Suche</button>
-			<p>{check}</p>
+            <button onClick={()=>{setSubmit(!checkSubmit)}}>Suche</button>
+			<p>{description} in {searchText}</p>
         </div>
-
+        <p>Aktuell: {temperature}°C</p>
+        <p>Windgeschwindigkeit: {windSpeed} m/std</p>
         </div>
 
     )
