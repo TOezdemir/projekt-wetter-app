16ef711b9d2c83dde3a7b346e0b7a72c5bc13f71
Tolgay Özdemir <tolgay.oezdemir@hsrw.org>
2024-10-15 15:40:16 +0200

    yes

--- src/components/WeatherPage.tsx
+++ src/components/WeatherPage.tsx
@@ -4,10 +4,10 @@ import { WiCloudy, WiDaySunny, WiRain } from "react-icons/wi";
 import { FaRegHeart } from "react-icons/fa";
 
 export default function WeatherPage(){
-	const [weatherData, setWeatherData] = useState<Weather>();
-    const [searchText, setSearchText] = useState("Berlin")
+	  const [weatherData, setWeatherData] = useState<Weather>();
+    const [searchText, setSearchText] = useState("")
     const [checkSubmit, setSubmit] = useState(false)
-	const language = "de";
+	  const language = "de";
     const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === "Enter") { 
           setSubmit(!checkSubmit);
@@ -27,30 +27,32 @@ export default function WeatherPage(){
 		} else {
 		  localStorage.setItem("lovedCities", JSON.stringify([lovedCity]));
 		}
-        // setSearchText("")    // Ah, das geht auch nicht, weil, wenn das Suchfeld geleert wird, gibt die Seite nichts aus,
-                                // also Blank. Müssen nen Failstate bauen, damit der im Fetch eine Standardausgabe hat?
 	}
 
+  const fetchWeather  = async () => setWeatherData(await FetchingFunction(searchText, language));
+
 	useEffect(() => {
-		const fetchWeather  = async () => setWeatherData(await FetchingFunction(searchText, language));
-        // setSearchText("")    // Haha - klappt ja nicht mehr, wegen des Bookmarkings! :D
 		fetchWeather();
 	}, [checkSubmit]);
 
-	const temperature = weatherData?.main.temp
+
+    const temperature = weatherData?.main.temp
     const description = weatherData?.weather[0].description
     const windSpeed = weatherData?.wind.speed
     const name = weatherData?.name
+  
+
+
     return(
-        <div className="font-sans flex flex-col items-center justify-center min-h-screen bg-blue-100"> 
-		<div className="m-5">
-		{buttonsLovedCitiesArray?.map((el: string) => {
-  			return (
+    <div className="font-sans flex flex-col items-center justify-center min-h-screen bg-blue-100"> 
+	  	<div className="m-5">
+		    {buttonsLovedCitiesArray?.map((el: string) => {
+  			  return (
     			<button key={el}
-				className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg m-5"
-				onClick={()=> {
-						setSearchText(el);
-						setSubmit(!checkSubmit);
+				  className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg m-5"
+				  onClick={()=> {
+					setSearchText(el);
+					fetchWeather();
 					}}
 				>{el}
 				</button>
@@ -58,48 +60,48 @@ export default function WeatherPage(){
 		})}
 		</div>
 		<div className="bg-white p-6 rounded-lg shadow-md">
-          <div className="mb-4">
-            <button
-            className="mr-4 text-2xl"
-			onClick={() => {safeCityQuerys(searchText); setSubmit(!checkSubmit)}}>
-            <FaRegHeart />
-			</button>
-            <input 
-              type="text" 
-              aria-label="Eingabefeld für Stadt" 
-              placeholder="Stadt"
-			  id="input" 
-              className="border border-gray-400 px-3 py-2 rounded-lg mr-2"
-              onChange={(event) => {
+        <div className="mb-4">
+        <button
+          className="mr-4 text-2xl"
+			    onClick={() => {safeCityQuerys(searchText); fetchWeather()}}>
+          <FaRegHeart />
+			  </button>
+        <input 
+          type="text" 
+          aria-label="Eingabefeld für Stadt" 
+          placeholder="Stadt"
+			    id="input" 
+          className="border border-gray-400 px-3 py-2 rounded-lg mr-2"
+          onChange={(event) => {
 					setSearchText(event.target.value);
 				}
 			  } 
-              value={searchText}
-			  onKeyDown={handleKeyDown}
-            />
-            <button 
-              className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg"
-              onClick={() => {setSubmit(!checkSubmit)
-			  }
+          value={searchText}
+			    onKeyDown={handleKeyDown}/>
+            
+          <button 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg"
+          onClick={() => {fetchWeather()}
 			}
-            >
-              Suche
-            </button>
+          >
+          Suche
+          </button>
           </div>
-          {weatherData && (
-            <div className="flex flex-col items-center">
-                <div className="text-6xl mb-4">
-                    {weatherData.weather[0].main === "Clear" && <WiDaySunny/>}
-                    {weatherData.weather[0].main === "Clouds" && <WiCloudy/>}
-                    {weatherData.weather[0].main === "Rain" && <WiRain/>}
-                </div>
+          {/* {weatherData && <WeatherData data={weatherData}/>} */}
+          {weatherData?.main && (
+          <div className="flex flex-col items-center">
+            <div className="text-6xl mb-4">
+              {weatherData.weather[0].main === "Clear" && <WiDaySunny/>}
+              {weatherData.weather[0].main === "Clouds" && <WiCloudy/>}
+              {weatherData.weather[0].main === "Rain" && <WiRain/>}
+            </div>
               <p className="text-xl font-semibold mb-2">
                 {description} in {name}
               </p>
               <p className="text-lg">Aktuell: {temperature}°C</p>
               <p className="text-lg">Windgeschwindigkeit: {windSpeed} m/std</p>
-            </div>
+          </div>
           )}
-        </div>
       </div>
-    )}
+    </div>
+)}
--- src/lib/FetchingFunction.tsx
+++ src/lib/FetchingFunction.tsx
@@ -17,10 +17,17 @@ export interface Weather{
     name: string
 } 
 
-export default async function FetchingFunction(query: string, lang: string): Promise<Weather> {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${appid}&units=metric&lang=${lang}`)
-    const data = await response.json() as Weather
-    console.log(data)
-    return data
+export default async function FetchingFunction(query: string, lang: string): Promise<Weather | undefined> {
+    try{    
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${appid}&units=metric&lang=${lang}`)
+        const data = await response.json() as Weather
+        console.log(data)
+        if(!data.main.temp){
+            throw new Error("Kapuuuutt")
+        }
+        return data
+    } catch(err){
+        console.error(err)
+    }
 }
 
